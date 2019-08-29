b7f755cedc681b70fe3916295943b09d83802b9d
Michael Hallquist <michael.hallquist@gmail.com>
2019-08-29 11:18:38 -0400

    Cleanup shutdown script

--- PsychToolbox/EL_shutdown.m
+++ PsychToolbox/EL_shutdown.m
@@ -1,26 +1,43 @@
 function EL_shutdown()
+% close data file and shut down tracker
+% inspired by: EyelinkPictureCustomCalibration.m
+% inspired by: https://psadil.gitlab.io/psadil/post/eyetracking-in-psychtoolbox-oop/
 
-% End of Experiment; close the file first
-% close graphics window, close data file and shut down tracker
-Eyelink('Command', 'set_idle_mode');
-WaitSecs(0.5);
-Eyelink('CloseFile');
+global el;
 
+
+% stop active recording (this should be done trialwise, but we may get here if an error is thrown and recording is ongoing)
+
+EyeLink('StopRecording');
+WaitSecs(0.2); % Slack to let stop definitely happen
+
+%set to idle mode and wait for mode switch to take effect
+%alternative that I think does the same thing: Eyelink('Command', 'set_idle_mode');
+EyeLink('SetOfflineMode'); 
+WaitSecs(0.2);
+
+EyeLink('CloseFile');
+WaitSecs(0.2);
+
+% TODO: Make sure that we can't overwrite and existing file!
 try
-    fprintf('Receiving data file ''%s''\n', edfFile);
-    status=Eyelink('ReceiveFile');
-    if status > 0
-        fprintf('ReceiveFile status %d\n', status);
-    end
-    if 2==exist(edfFile, 'file')
-        fprintf('Data file ''%s'' can be found in ''%s''\n', edfFile, pwd );
+    %receive file from eye tracker    
+    fprintf('Receiving data file ''%s''\n', el.edf_file);
+    
+    %put the edf in the current working directory
+    status = EyeLink('ReceiveFile', el.edf_file, fullfile(pwd, el.edf_file), 1);
+    WaitSecs(0.2);
+
+    if status > 0, fprintf('ReceiveFile status %d\n', status); end
+        
+    if 2==exist(el.edf_file, 'file')
+        fprintf('Data file ''%s'' can be found in ''%s''\n', el.edf_file, pwd);
     end
 catch %#ok<*CTCH>
-    fprintf('Problem receiving data file ''%s''\n', edfFile );
+    fprintf('Problem receiving data file ''%s''\n', el.edf_file);
 end
 
 % Shutdown Eyelink:
 Eyelink('Shutdown');
-
 
 end
