e75dad80f632ffd1ddeba1be4d18890dedef9ac1
Nathan Tyler Hall <nth7@psu.edu>
2019-08-29 11:37:21 -0400

    fix sendScreenEL mixup

--- PsychToolbox/sendScreenEL.m
+++ PsychToolbox/sendScreenEL.m
@@ -1,4 +1,4 @@
-function sendScreenEL(stim, block, trial, approach, el, wd, width, height, stimondelay, aois, aoi_labs, clear_stim, end_trial)
+function sendScreenEL(screen_export)
 % This function sends information about stimuli being currently presented
 % to an .edf file. Also sets up area of interest info for static areas of interest.
 % 
@@ -12,70 +12,38 @@ function sendScreenEL(stim, block, trial, approach, el, wd, width, height, stimo
 %             {4}: height of the screen
 %             {5}: aoi coordinates that are stored in a cell. these must contain x0,y0,x1,y1 coordinates for data viewer to recognize them
 %             {6}: aoi labels that are stored in a cell of the same length as {5}
- 
-id_str=sprintf('%s_%s_%d_%s', stim, block, trial, approach) 
-     
+
+
+
  %take screenshot, write, then send to EDF file for data viewer compatibility   
- img=Screen('GetImage', wd, [0 0 1920 1080]);
+ img=Screen('GetImage', screen_export{1}, [0 0 screen_export{3} screen_export{4}]);
   
-    imwrite(img, sprintf('screenshots/on_%s.jpeg', id_str));
-    finfo = imfinfo(sprintf('screenshots/on_%s.jpeg', id_str));
-    finfo.Filename;  %just spits out file name, kinda useless 
+    imwrite(img, sprintf('screenshots/on_%s.jpeg', screen_export{2}));
+    finfo = imfinfo(sprintf('screenshots/on_%s.jpeg', screen_export{2}));
+%     finfo.Filename;  %just spits out file name, kinda useless 
     %Sends Image to Data Viewer
-    Eyelink('Message', '!V IMGLOAD CENTER %s %d %d', finfo.Filename, width/2, height/2);
+    Eyelink('Message', '!V IMGLOAD CENTER %s %d %d', finfo.Filename, screen_export{3}/2, screen_export{4}/2);
     
     Eyelink('command', 'ImageTransfer', finfo.Filename)
-    WaitSecs(stimondelay)
-    
-%     if(clear_stim);
-%      % Clear the display
-%         Screen('FillRect', wd, el.backgroundcolour);
-%         Screen('Flip', wd);
-%     end
-%         %send messages regardless, since the next time we send the screen 
-%         if end_trial
-%             Eyelink('Message', 'ENDTIME');
-%         end
-%      Status = Eyelink('ImageTransfer', finfo.Filename);
-%          if transferStatus ~= 0
-%              fprintf('Image to host transfer failed\n');
-%          end
-%         WaitSecs(0.1);
-       % Eyelink('command', 'draw_box_%d_%d_%d_%d', width/2-50, height/2-50, width/2+50, height/2+50)  
-        %%draw areas of interest. Flexible to number of rectangles. Just pass a
-    %%n_aoi x 4 double in as aois, make sure aoi_labs== rows of aois (i.e.
-    %%number of aois)
-    
-    dims=size(aois);
-    n_aois=dims(1); %number of AOIs we need to draw, should match length of aoi_labs
-    %aoi_labs = {'fractal'}
 
-    %N.B. important when only one AOI is being passed in that it is a
-    %single-cell array
-%     dim_labs = size(aoi_labs);
-%     n_labs = dim_labs(1)
-    
-    status=length(aoi_labs)==n_aois;
-    if aois~=0    
+
+
+%% create actual interest area depending on dimensions of input
+
+    dims=size(screen_export{5});
+    n_aois=dims(1); %number of AOIs we need to draw, should match length of aoi_labs
+
+    status=length((screen_export{6})==n_aois;
+    if screen_export{5}~=0    
     if status ~= 0 
         for aoi=1:n_aois
-            these_dims = aois(aoi,:)
-            Eyelink('Message', '!V IAREA RECTANGLE %d %d %d %d %d %s', aoi, these_dims(1), these_dims(2), these_dims(3), these_dims(4), aoi_labs{aoi}); 
-%             WaitSecs(.1);
-%             Eyelink('Message', '%s_%d_%d_%d_%d_%d ', aoi_labs{aoi}, trial, these_dims(1), these_dims(2), these_dims(3), these_dims(4));
-            
+            these_dims = screen_export{5}(aoi,:)
+            Eyelink('Message', '!V IAREA RECTANGLE %d %d %d %d %d %s', aoi, these_dims(1), these_dims(2), these_dims(3), these_dims(4), screen_export{6}{aoi}); 
         end
     else
         error('aois df does not match labels!')
     end
     end
     
-%     WaitSecs(.1) 
-%     
-%     if end_trial;
-%         Eyelink('Message', 'TRIAL_RESULT 0')
-%         Eyelink('StopRecording');
-%     end
-      
-end
 
+end
